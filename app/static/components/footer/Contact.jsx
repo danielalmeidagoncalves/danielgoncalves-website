3fdeb1d2c27081be0c7a5ec56ff295f20742e476
Daniel Gonçalves <daniel.deeds@gmail.com>
2016-02-19 23:58:45 +0000

    adding contact form

--- app/static/components/footer/Contact.jsx
+++ app/static/components/footer/Contact.jsx
@@ -10,37 +10,8 @@ define(function(require, exports, module) {
       return (
           <div className="ui container">
             <div className="ui stackable inverted divided equal height stackable grid">
-              <div className="three wide column">
-                <h4 className="ui inverted header">About</h4>
-                <div className="ui inverted link list">
-                  <a href="#" className="item">Sitemap</a>
-                  <a href="#" className="item">
-                    Contact Us
-                  </a>
-                  <a href="#" className="item">
-                    Religious Ceremonies
-                  </a>
-                  <a href="#" className="item">
-                    Gazebo Plans
-                  </a>
-                </div>
-              </div>
-              <div className="three wide column">
-                <h4 className="ui inverted header">Services</h4>
-                <div className="ui inverted link list">
-                  <a href="#" className="item">
-                    Banana Pre-Order
-                  </a>
-                  <a href="#" className="item">
-                    DNA FAQ
-                  </a>
-                  <a href="#" className="item">
-                    How To Access
-                  </a>
-                  <a href="#" className="item">
-                    Favorite X-Men
-                  </a>
-                </div>
+              <div className="eight wide column">
+                <img className="ui fluid image" src="/static/imgs/waldo.png" />
               </div>
               <div className="seven wide column">
                 <h4 className="ui inverted header">
