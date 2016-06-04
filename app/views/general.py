7acfb24d985a42360f51078593cd0257c7e526d8
Daniel Almeida Gonçalves <daniel@daniel.home>
2016-06-04 14:36:30 +0100

    Adding google verification code

--- app/views/general.py
+++ app/views/general.py
@@ -11,6 +11,12 @@ def index():
     return render_template('index.html')
 
 
+@general.route('/google3a491a2ba06afd19.html')
+@nocache
+def google_verify():
+    return render_template('google3a491a2ba06afd19.html')
+
+
 # Sample HTTP error handling
 @general.errorhandler(404)
 def not_found(error):
